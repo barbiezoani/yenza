ab1475d6b8c910932a2449b549922d1b355bf54b
sol <sbuker@itba.edu.ar>
2022-04-16 15:31:28 -0300

    lang

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'fr', to: 'pages#home-fr'
+  get 'de', to: 'pages#home-de'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
